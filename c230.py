3ade66645356852e413c760de3886b7f1706ff76
aswinganji <64272381+aswinganji@users.noreply.github.com>
2021-09-23 16:47:47 +0530

    Add files via upload

---
+++ c230.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+dataset = pd.read_csv(r'countries_vaccination.csv')
+
+print("Shape of given dataset:",dataset.shape)
+
+print("Count of column:",len(dataset.columns)
+
+print("Name of parameter used:",dataset.columns)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
